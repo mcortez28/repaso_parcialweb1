0bf7249d98ccff6d6a48b05bab40849bf1c2636b
Carlos Moreno <carmorenoc@unal.edu.co>
2026-03-16 16:52:25 -0500

    hola camila

--- funciones.js
+++ funciones.js
@@ -1,3 +1,3 @@
 function agregartxt(){
-    var a = document.getElementById("camilaxd").innerHTML("xd");
+    var a = document.getElementById("camilaxd").innerHTML+= "<p>corregido para camila</p>";
 }
