dab386868ce28b438dffd8aa509f3542266c2674
mcortez28 <mcorteza@unal.edu.co>
2026-03-16 17:02:40 -0500

    aaa

--- funciones.js
+++ funciones.js
@@ -1,3 +1,6 @@
 function agregartxt(){
     var a = document.getElementById("camilaxd").innerHTML= "<p>corregido para camila</p>";
 }
+function clicktxt(){
+    var b = document.getElementById("abc").innerHTML = "<div>Click botón</div>"
+}
